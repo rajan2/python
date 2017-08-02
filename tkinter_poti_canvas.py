4a22be81a995d887560c3a072b7004edc99ea574
rajan2 <rajan2@users.noreply.github.com>
2017-08-02 15:53:22 +0200

    Update tkinter_poti_canvas.py

--- tkinter_poti_canvas.py
+++ tkinter_poti_canvas.py
@@ -1,4 +1,9 @@
+import Adafruit_GPIO as GPIO
+import Adafruit_GPIO.SPI as SPI
 from Tkinter import *
+import time
+import Adafruit_MCP3008
+
 #List to store values
 my_list = []
 # Front lables to display
@@ -7,6 +12,19 @@ flabels = []
 alabels = []
 # Dummy list generator incrementor
 i = 1
+i
+#initialize MCP
+
+# Software SPI configuration:
+CLK  = 11
+MISO = 9
+MOSI = 10
+CS   = 8
+mcp = Adafruit_MCP3008.MCP3008(clk=CLK, cs=CS, miso=MISO, mosi=MOSI)
+print('Reading MCP3008 values, press Ctrl-C to quit...')
+# Print nice channel column headers.
+print('| {0:>4} |'.format(*range(1)))
+print('-' * 65)
 
 def start_app():
     """
@@ -37,12 +55,39 @@ def dummy_list_gen():
     """
     Dummy List generator
     """
-    global i
-    my_list = [3,2,3,4,5]
-    my_list = [i] * 5
-    print my_list
-    i += 1
-    return my_list
+    #global i
+    #my_list = [3,2,3,4,5]
+    #my_list = [i] * 5
+    #print my_list
+    #i += 1
+    #return my_list
+
+    # Read all the ADC channel values in a list.
+    values = [0]*5
+    volt = [0]*5
+    curr = [0]*5
+    resis = [0]*5
+    v = 3.278
+    bit = 1023
+    for i in range(5):
+        # The read_adc function will get the value of the specified channel (0-7).
+        values[i] = (mcp.read_adc(i) * v / bit)
+        print(mcp.read_adc(i))
+        volt = values[:]
+        #values[i] = round(values[i], 3)
+        values[i] = v - values[i]
+        curr[i] = values[i] / 11000
+        resis[i] = volt[i] / curr[i]
+        if resis[i] == 0:
+            continue
+        else:
+            resis[i] = resis[i]
+            resis[i] = resis[i] / 1000
+            resis[i] = round(resis[i], 2)
+        # Print the ADC values.
+    print('| {0:>4} KOhm | {1:>4} KOhm | {2:>4} KOhm | {3:>4} KOhm | {4:>4} KOhm |'.format(*resis))
+    return resis
+
 
 def update_label_values(my_list):
     """
